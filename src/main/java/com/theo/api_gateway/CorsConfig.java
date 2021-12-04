100c998d981d68bc3b87d190ea555ced17c68e12
Theodoros Ioakeimidis <theioak@iti.gr>
2021-12-04 18:57:17 +0200

    Added CORS configuration

---
+++ src/main/java/com/theo/api_gateway/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.theo.api_gateway;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class CorsConfig implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
