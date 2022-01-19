6196c224f1a1e29765dd8846544d862be33a6176
Philippe Krief <philippe.krief@eclipse-foundation.org>
2022-01-20 00:40:59 +0100

    Adding missing copyrights
    Signed-off-by: Philippe Krief <philippe.krief@eclipse-foundation.org>

--- src/test/java/com/theo/api_gateway/SpringCloudGatewaySecurityApplicationTests.java
+++ src/test/java/com/theo/api_gateway/SpringCloudGatewaySecurityApplicationTests.java
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2021 CERTH
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     theioakiti - initial API and implementation
+ *******************************************************************************/
 package com.theo.api_gateway;
 
 import org.junit.jupiter.api.Test;
