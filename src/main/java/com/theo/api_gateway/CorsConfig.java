c04cf5e6c167507ec414e43ddde41222ad878f9a
theioak <theioak@iti.gr>
2022-08-31 12:29:45 +0300

    Changed CORS configuration

--- src/main/java/com/theo/api_gateway/CorsConfig.java
+++ src/main/java/com/theo/api_gateway/CorsConfig.java
@@ -1,30 +1,30 @@
-/*******************************************************************************
- * Copyright (c) 2021 CERTH
- *
- * This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License 2.0
- * which accompanies this distribution, and is available at
- * https://www.eclipse.org/legal/epl-2.0/
- *
- * SPDX-License-Identifier: EPL-2.0
- *
- * Contributors:
- *     theioakiti - initial API and implementation
- *******************************************************************************/
-package com.theo.api_gateway;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.config.CorsRegistry;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
-
-@Configuration
-public class CorsConfig implements WebFluxConfigurer {
-
-    @Override
-    public void addCorsMappings(final CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*")
-                .allowedHeaders("*");
-    }
-}
+///*******************************************************************************
+// * Copyright (c) 2021 CERTH
+// *
+// * This program and the accompanying materials
+// * are made available under the terms of the Eclipse Public License 2.0
+// * which accompanies this distribution, and is available at
+// * https://www.eclipse.org/legal/epl-2.0/
+// *
+// * SPDX-License-Identifier: EPL-2.0
+// *
+// * Contributors:
+// *     theioakiti - initial API and implementation
+// *******************************************************************************/
+//package com.theo.api_gateway;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.reactive.config.CorsRegistry;
+//import org.springframework.web.reactive.config.WebFluxConfigurer;
+//
+//@Configuration
+//public class CorsConfig implements WebFluxConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(final CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowedMethods("*")
+//                .allowedHeaders("*");
+//    }
+//}
